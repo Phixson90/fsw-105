6d514fc7fb5fc42023ad8a6c4d83bd1513e8d755
Phixson90 <67671387+Phixson90@users.noreply.github.com>
2020-08-29 22:19:58 -0700

    Add files via upload

---
+++ pAndPDataStructuresAndVariables/index.js
@@ -0,0 +1,10 @@
+var shopper = {
+    name = "Tiffany",
+    age = 29,
+    paidWithCash = true,
+    groceryCart = ["steak, bread, lettuce, soda"],
+    nameAndAge : function () {
+        return this. name + "" + this.age;
+    }
+
+}
