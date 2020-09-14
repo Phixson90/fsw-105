597fc5a9c8c5637de993a2207874e28d8484f442
Phixson90 <67671387+Phixson90@users.noreply.github.com>
2020-09-14 11:06:57 -0700

    Add files via upload

--- fsw-105/week2/if_statement_olympics/index.js
+++ fsw-105/week2/if_statement_olympics/index.js
@@ -1,71 +1,71 @@
-// if(5 > 3) {
-//     console.log("Is greater than")
-// }
+if(5 > 3) {
+    console.log("Is greater than")
+}
+var str  = "cat" 
+    if("cat".length === 3){
+    console.log("is the length")
+    }
+
+if("cat" === "dog") {
+    console.log("The Same")
+}   else {
+        console.log("not the same")
+}
+
+var person = {
+    name: "Bobby",
+    age: 12
+}
+
+if(person.age >= 18) {
+    console.log("Allowed into movie")
+}   else {
+    console.log("Not allowed into movie")
+}
+
+if(person.name.startsWith("B") ) {
+    console.log("Is allowed into movie")
+}   else {
+    console.log("is not allowed into movie")
+}
+
+if(person.age >= 18 && person.name.startsWith("B")) {
+    console.log("Is allowed into movie")
+}   else {
+    console.log("Is not allowed into movie")
+}
 
 
+if ("1" === 1) {
+    console.log("strictly")
+}
+else if ("1" == 1) {
+    console.log("loose")
+}
+else if ("1" !== 1) {
+    console.log("not equal at all")
+}
 
 
-// if("cat" === "dog") {
-//     console.log("The Same")
-// }   else {
-//         console.log("not the same")
-// }
+if (1 <= 2 && 2 === 4) {
+    console.log("yes")
+}
+else {
+    console.log("no")
+}
 
-// var person = {
-//     name: "Bobby",
-//     age: 12
-// }
-
-// if(person.age >= 18) {
-//     console.log("Allowed into movie")
-// }   else {
-//     console.log("Not allowed into movie")
-// }
-
-// if(person.name.startsWith("B") ) {
-//     console.log("Is allowed into movie")
-// }   else {
-//     console.log("is not allowed into movie")
-// }
-
-// if(person.age >= 18 && person.name.startsWith("B")) {
-//     console.log("Is allowed into movie")
-// }   else {
-//     console.log("Is not allowed into movie")
-// }
+if ("typeOfDog" === "string") {
+    console.log("dog is equal to string")
+}
+else {
+    console.log("dog is not equal to string")
+}
 
 
-// if ("1" === 1) {
-//     console.log("strictly")
-// }
-// else if ("1" == 1) {
-//     console.log("loose")
-// }
-// else if ("1" !== 1) {
-//     console.log("not equal at all")
-// }
-
-
-// if (1 <= 2 && 2 === 4) {
-//     console.log("yes")
-// }
-// else {
-//     console.log("no")
-// }
-
-
-// if (typeOf Dog === "string") {
-//     console.log("dog is equal to string")
-// }
-// else {
-//     console.log("dog is not equal to string")
-// }
-
-
-// if (true) {
-//     console.log("is a boolean")
-// }
-// else {
-//     console.log("is not a boolean")
-// }
+if (typeof true === "boolean") {
+    console.log("is a boolean")
+}
+else {
+    console.log("is not a boolean")
+}
 
